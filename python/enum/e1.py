b111e02aa3ec51fbe380852565effbbf640a0796
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-08 14:31:58 +0330

    .

---
+++ python/enum/e1.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Color(Enum):
+    red = 'w23wdsd'
+    yellow = 'w2wfwdsd'
+    blue = 'w77adswdsfdsd'
+
+
+print(Color.red.name) # red
+print(Color.red.value) # w23wdsd
