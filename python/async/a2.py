0225309fee0ad50629897048d93fd776bd8b8794
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-15 19:22:25 +0330

    .

---
+++ python/async/a2.py
@@ -0,0 +1,25 @@
+import asyncio
+
+
+async def nested():
+    return 42
+
+
+def normal():
+    return 45
+
+
+async def main():
+    nested()  # RuntimeWarning: coroutine 'nested' was never awaited
+    print(await nested())  # will print "42".
+
+
+asyncio.run(main())
+
+
+async def main():
+    # print(normal())  # will print 45
+    # print(await normal())  # TypeError: object int can't be used in 'await' expression
+
+
+asyncio.run(main())
