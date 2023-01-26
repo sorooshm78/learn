9c73d2b7f417c8bf0a0059830f2d0f1a55ec82e8
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-26 17:57:50 +0330

    .

--- python/numpy/e4.py
+++ python/numpy/e4.py
@@ -20,13 +20,55 @@ ship = "x"
 matrix = np.arange(100).reshape(row, col)
 
 
-point = Point(slice(0, 2), slice(0, 1))
+def get_list_of_point(points):
+    list_point = []
+    for x in range(points.x.start, points.x.stop):
+        for y in range(points.y.start, points.y.stop):
+            list_point.append(Point(x, y))
+
+    return list_point
+
+
+########################################
+
+# point = Point(slice(0, 2), slice(0, 1))
+
+# points = Point(
+#     slice(max(0, point.x.start - 1), max(0, point.x.stop + 1)),
+#     slice(max(0, point.y.start - 1), max(0, point.y.stop + 1)),
+# )
+
+# print(matrix[points.x, points.y])
+# print(matrix)
+
+########################################
+
+# point = Point(9, 9)
+
+# print(matrix[point.x, point.y])
+
+
+# points = Point(
+#     slice(max(0, point.x - 1), min(col, point.x + 2)),
+#     slice(max(0, point.y - 1), min(row, point.y + 2)),
+# )
+# print(matrix[points.x, points.y])
+# print(matrix)
+# for p in get_list_of_point(points):
+#     print(p)
+
+########################################
+
+point = Point(9, 9)
+
 print(matrix[point.x, point.y])
 
-point = Point(
-    slice(max(0, point.x.start - 1), max(0, point.x.stop + 1)),
-    slice(max(0, point.y.start - 1), max(0, point.y.stop + 1)),
+
+points = Point(
+    slice(point.x, point.x + 1),
+    slice(0, row),
 )
-
-print(matrix[point.x, point.y])
+print(matrix[points.x, points.y])
 print(matrix)
+for p in get_list_of_point(points):
+    print(p)
