5651ba53afe9eb0f1d11f6e8e3284796855af64f
Soroush mohammadi <sorooshm78@gmail.com>
2023-02-08 16:59:37 +0330

    .

--- python/async/a9.py
+++ python/async/a9.py
@@ -176,3 +176,53 @@ asyncio.run(main())
 # after loop in pending 16:45:37
 # pending ['task3', 'task1']
 # done {<Task finished name='Task-3' coro=<say_after() done, defined at /home/sm/src/learn/python/async/a9.py:139> exception=Exception('something')>}
+
+#######################
+
+async def say_after(delay, what):
+    print(f"befor sleep {what}", time.strftime("%X"))
+    await asyncio.sleep(delay)
+    print(f"after sleep {what}", time.strftime("%X"))
+    return what
+
+
+async def main():
+    tasks = [
+        asyncio.create_task(say_after(5, "task5")),
+        asyncio.create_task(say_after(4, "task4")),
+        asyncio.create_task(say_after(2, "task2")),
+        asyncio.create_task(say_after(1, "task1")),
+    ]
+
+    print("before wait main", time.strftime("%X"))
+    done, pending = await asyncio.wait(tasks, timeout=3)
+    print("after wait main", time.strftime("%X"))
+    
+    print("done", done)
+    print("pending", pending)
+
+    print("before loop in pending", time.strftime("%X"))
+    for p in pending:
+        await p
+    print("after loop in pending", time.strftime("%X"))
+
+    print("pending", [p.result() for p in pending])
+    print("done", done)
+
+asyncio.run(main())
+# before wait main 16:57:03
+# befor sleep task5 16:57:03
+# befor sleep task4 16:57:03
+# befor sleep task2 16:57:03
+# befor sleep task1 16:57:03
+# after sleep task1 16:57:04
+# after sleep task2 16:57:05
+# after wait main 16:57:06
+# done {<Task finished name='Task-5' coro=<say_after() done, defined at /home/sm/src/learn/python/async/a9.py:182> result='task1'>, <Task finished name='Task-4' coro=<say_after() done, defined at /home/sm/src/learn/python/async/a9.py:182> result='task2'>}
+# pending {<Task pending name='Task-3' coro=<say_after() running at /home/sm/src/learn/python/async/a9.py:184> wait_for=<Future pending cb=[Task.task_wakeup()]>>, <Task pending name='Task-2' coro=<say_after() running at /home/sm/src/learn/python/async/a9.py:184> wait_for=<Future pending cb=[Task.task_wakeup()]>>}
+# before loop in pending 16:57:06
+# after sleep task4 16:57:07
+# after sleep task5 16:57:08
+# after loop in pending 16:57:08
+# pending ['task4', 'task5']
+# done {<Task finished name='Task-5' coro=<say_after() done, defined at /home/sm/src/learn/python/async/a9.py:182> result='task1'>, <Task finished name='Task-4' coro=<say_after() done, defined at /home/sm/src/learn/python/async/a9.py:182> result='task2'>}
