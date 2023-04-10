9d60335a04d2bdfea907829bb897ad8c44d057c8
soroosh <sorooshm78@gmail.com>
2023-04-10 21:18:49 +0330

    .

---
+++ django/model_inheritance/multi_table/command.py
@@ -0,0 +1,24 @@
+from multi_table.models import Location, Restaurant
+
+
+# tables create:
+#    - Location
+#    - Restaurant
+
+
+Location.objects.create(name="l", address="adrr")
+# id  | name | address
+# 1   | l    | "adrr"
+
+
+Restaurant.objects.create(name="Rest", address="addr2", star=5)
+
+# location table
+# id  | name | address
+# 1   | l    | "adrr"
+# 2   | Rest | "addr2"
+
+
+# restaurant table
+# location_ptr_id | star
+#  2              | 5
