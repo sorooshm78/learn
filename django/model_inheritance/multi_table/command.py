42561f95ca6aa0dc0b5374c1d7cc842cdb657142
soroosh <sorooshm78@gmail.com>
2023-04-12 13:03:45 +0330

    .

--- django/model_inheritance/multi_table/command.py
+++ django/model_inheritance/multi_table/command.py
@@ -6,19 +6,90 @@ from multi_table.models import Location, Restaurant
 #    - Restaurant
 
 
-Location.objects.create(name="l", address="adrr")
+# -------------------------------------------
+
+Location.objects.create(name="loc", address="adrr")
 # id  | name | address
 # 1   | l    | "adrr"
 
+# -------------------------------------------
 
 Restaurant.objects.create(name="Rest", address="addr2", star=5)
 
 # location table
 # id  | name | address
-# 1   | l    | "adrr"
+# 1   | loc    | "adrr"
 # 2   | Rest | "addr2"
 
 
 # restaurant table
 # location_ptr_id | star
 #  2              | 5
+
+# -------------------------------------------
+r = Restaurant.objects.filter(name="Rest")[0]
+
+r.name
+# Rest
+
+r.address
+# 'addr2'
+
+r.star
+# 5
+
+# -------------------------------------------
+
+# location table
+# id  | name | address
+# 1   | loc    | "adrr"
+# 2   | Rest | "addr2"
+
+# restaurant table
+# location_ptr_id | star
+#  2              | 5
+
+Restaurant.objects.create(location_ptr_id=1, star=10)
+# <Restaurant: Restaurant object (1)>
+
+# restaurant table
+# location_ptr_id | star
+#  2              | 5
+#  1              | 10
+
+
+Restaurant.objects.create(location_ptr_id=1, star=66)
+# Erorr -> relation is one to one
+
+# -------------------------------------------
+
+# location table
+# id  | name | address
+# 1   | loc    | "adrr"
+# 2   | Rest | "addr2"
+
+# restaurant table
+# location_ptr_id | star
+#  2              | 5
+#  1              | 10
+
+Location.objects.get(id=1).delete()
+# on-delete is CASCADE
+
+# location table
+# id  | name | address
+# 2   | Rest | "addr2"
+
+# restaurant table
+# location_ptr_id | star
+#  1              | 10
+
+# -------------------------------------------
+
+Location.objects.get(id=2)
+# <Location: Rest - addr2>
+
+Restaurant.objects.get(id=2)
+# <Restaurant: Rest - addr2 - 5>
+
+# -------------------------------------------
--- django/model_inheritance/multi_table/models.py
+++ django/model_inheritance/multi_table/models.py
@@ -5,6 +5,12 @@ class Location(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
 
+    def __str__(self):
+        return f"{self.name} - {self.address}"
+
 
 class Restaurant(Location):
     star = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.address} - {self.star}"
