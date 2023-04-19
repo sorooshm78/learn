a99ee95447e32c87e21ae89fe7db084d29e0e5f9
soroosh <sorooshm78@gmail.com>
2023-04-19 20:31:59 +0330

    .

--- python/context_manager.py
+++ python/context_manager.py
@@ -5,6 +5,16 @@
 # In Python, it can be achieved by the usage of context managers which facilitate the proper handling of resources.
 # The most common way of performing file operations is by using the keyword as shown below:
 
+# .__enter__() is called by the with statement to enter the runtime context.
+# .__exit__() is called when the execution leaves the with code block.
+
+
+file = open("hello.txt", "w")
+try:
+    file.write("Hello, World!")
+finally:
+    file.close()
+
 
 with open("test.txt") as f:
     data = f.read()
