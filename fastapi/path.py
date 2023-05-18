b50ae899b81dfcdf3b7ad646a4ef87fd358ce2b9
soroosh <sorooshm78@gmail.com>
2023-05-18 17:18:06 +0330

    .

--- fastapi/path.py
+++ fastapi/path.py
@@ -79,3 +79,23 @@ async def get_city(city_name: CityName):
     return {"city": "city of Iran"}
 
 
+# --------------------------------------
+
+from typing import Annotated
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+# --------------------------------------
+
